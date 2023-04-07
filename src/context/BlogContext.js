e4b784ec4df3591cad53513921fbdec3f7e94cad
Adeyinka Junaid <yinkajunn@gmail.com>
2023-04-07 13:47:24 +0100

    Extract context creation logic to separate file

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext({})
 
-export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>
-        {children}
-    </BlogContext.Provider>
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post #${state.length + 1}` }]
+        default:
+            return state
+    }
+}
+const addBlogPost = (dispatch) => {
+    return () => {
+        dispatch({ type: 'add_blogpost' })
+    }
 }
 
-export default BlogContext 
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, [])
+
+// const BlogContext = React.createContext({})
+
+// const blogReducer = (state, action) => {
+//     switch (action.type) {
+//         case 'add_blogpost':
+//             return [...state, { title: `Blog Post #${state.length + 1}` }]
+//         default:
+//             return state
+//     }
+// }
+
+// export const BlogProvider = ({ children }) => {
+//     // const [blogPosts, setBlogPosts] = useState([])
+
+//     const [blogPosts, dispatch] = useReducer(blogReducer, [])
+
+//     const addBlogPost = () => {
+//         dispatch({type:'add_blogpost'})
+//     }
+
+//     return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+//         {children}
+//     </BlogContext.Provider>
+// }
+
+// export default BlogContext  
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,17 +1,29 @@
 import React, { useContext } from "react";
-import { StyleSheet } from "react-native";
+import { Button, FlatList, ScrollView, StyleSheet } from "react-native";
 import { Text } from "react-native";
 import { View } from "react-native";
-import BlogContext from "../context/BlogContext";
+// import BlogContext from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
+import {Feather} from '@expo/vector-icons'
 
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext)
+    const { state, addBlogPost } = useContext(Context)
     return (
         <View>
-            <Text>Index Screen</Text>
-            <Text>{value}</Text>
+            <Button title="Add post" onPress={addBlogPost} />
+            <FlatList
+                data={state}
+                keyExtractor={blogPost => blogPost.title}
+                renderItem={({ item }) => {
+                    return <View>
+                        <Text>{item.title}</Text>
+                        
+                    </View>
+                }}
+            />
         </View>
+
     )
 }
 
